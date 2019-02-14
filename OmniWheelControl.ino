e824512a13d1e96515fff89d44c931e17eca7bb1
Jan Kolkmeier <jankolkmeier@gmail.com>
2019-02-14 19:31:13 +0100

    new version

--- OmniWheelControl.ino
+++ OmniWheelControl.ino
@@ -1,8 +1,8 @@
 #include <math.h>
 #include <WickedMotorShield.h>
-Wicked_DCMotor m0(M1);
-Wicked_DCMotor m1(M2);
-Wicked_DCMotor m2(M3);
+Wicked_DCMotor m1(M1);
+Wicked_DCMotor m2(M2);
+Wicked_DCMotor m3(M3);
 
 #define RC1 4 //Radio control inputs
 #define RC2 8 //Let's avoid magic magic numbers
@@ -17,84 +17,61 @@ void setup() {
   pinMode(RC1, INPUT);
   pinMode(RC2, INPUT);
   
-  m0.setBrake(BRAKE_OFF);
   m1.setBrake(BRAKE_OFF);
   m2.setBrake(BRAKE_OFF);
+  m3.setBrake(BRAKE_OFF);
 }
 
+
 void loop(){
-  //STEP 1: Read in the RC values
-  ch1 = pulseIn(4, HIGH, 25000);
-  ch2 = pulseIn(8, HIGH, 25000);
-  
-  ch1 = ch1 - 1500; //center it at 0
-  ch2 = ch2 - 1500; //for range 1000 - 2000
+  // Time in seconds
+  float s = (float) millis() / 1000.0f;
+
+  // Modulate angular motion over time
+  setMotors(300, 0, 100*sin(s*2));
+
+  // Modulate speed over time (driving straight)
+  //setMotors(550 + 450*sin(t*5), 0, 0);
+}
+
+
+/* Parameters:
+ *  magnitude: how fast to move in the direction (a value between 0 and 1000)
+ *  dir: angle of the direction (in degrees) relative to the robot orientation.
+ *  rot: angular motion (a value between 0 and 1000)
+ *  
+ *  !!! TODO: right now, the sum of rot and magnitude should not be bigger than 1000.
+ */
+void setMotors(float magnitude, float dir, float angularMotion) {
+
+    // TODO: if (rot + magnitude > 1000) normalize/scale the two?
     
-  if(ch1 < -500) ch1 = 0;
-  if(ch2 < -500) ch2 = 0;
-  
-  //STEP 2. Convert them to a direction vector
-  float theta = atan2(ch1, ch2);
-  float magnitude = sqrt((ch1*ch1)+(ch2*ch2));
-  
-  // magnitudes less than about 50 are neutral stick positions
-  // and should probably just be ignored
-  if(magnitude > 75.0f){  
+    // Formula from http://citeseerx.ist.psu.edu/viewdoc/download?doi=10.1.1.99.1083&rep=rep1&type=pdf
+    float w1 = magnitude * sin(rad(  0 - dir)) + angularMotion;
+    float w2 = magnitude * sin(rad(120 - dir)) + angularMotion;
+    float w3 = magnitude * sin(rad(240 - dir)) + angularMotion;
+    // 0 / 120 / 240 define the frame of reference of the robot.
+    // i.e. setting dir to 0 means that motor 1 won't turn / we're turning in direction of motor 1.
+
+    m1.setDirection(w1 < 0 ? DIR_CCW : DIR_CW); // If w0 is smaller than 0, motor goes CCW=counterclockwise..
+    m2.setDirection(w2 > 0 ? DIR_CCW : DIR_CW); // For w1 we changed the sign to get it to rotate in the correct direction
+    m3.setDirection(w3 < 0 ? DIR_CCW : DIR_CW); // ...
+
+    // Map the computed motor speed to the control range.
+    // i.e. a value of 1000 gets mapped to the maximum motor speed of 255.
+    byte w1_speed = (byte) map(abs(w1), 0, 1000, 0, 255);
+    byte w2_speed = (byte) map(abs(w2), 0, 1000, 0, 255);
+    byte w3_speed = (byte) map(abs(w3), 0, 1000, 0, 255);
     
-    Serial.print("ch1: ");
-    Serial.print(ch1);
-    Serial.print(" ch2: ");
-    Serial.print(ch2);
-  
-    Serial.print(" Angle: ");
-    Serial.print(degrees(theta));
-  
-    Serial.print(" Magnitude: ");
-    Serial.print(magnitude);  
-    
-    float vx = magnitude * cos(theta);
-    float vy = magnitude * sin(theta);
-    const float sqrt3o2 = 1.0*sqrt(3)/2;
-    //STEP 3. Find wheel vecotrs
-    float w0 = -vx;                   // v dot [-1, 0] / 25mm
-    float w1 = 0.5*vx - sqrt3o2 * vy; // v dot [1/2, -sqrt(3)/2] / 25mm
-    float w2 = 0.5*vx + sqrt3o2 * vy; // v dot [1/2, +sqrt(3)/2] / 25mm
-   
-    boolean w0_ccw = w0 < 0 ? true : false;
-    boolean w1_ccw = w1 < 0 ? true : false;
-    boolean w2_ccw = w2 < 0 ? true : false;
-    byte w0_speed = (byte) map(abs(w0), 0, 600, 0, 255);
-    byte w1_speed = (byte) map(abs(w1), 0, 600, 0, 255);
-    byte w2_speed = (byte) map(abs(w2), 0, 600, 0, 255);
-   
-    Serial.print(" vx: ");
-    Serial.print(vx);
-    Serial.print(" vy: ");
-    Serial.print(vy);
-    
-    Serial.print(" w0: ");
-    Serial.print(w0_speed);
-    if(w0_ccw) Serial.print(" CCW"); else Serial.print(" CW");
-    Serial.print(" w1: ");
-    Serial.print(w1_speed);
-    if(w1_ccw) Serial.print(" CCW"); else Serial.print(" CW");
-    Serial.print(" w2: ");
-    Serial.print(w2_speed);
-    if(w2_ccw) Serial.print(" CCW"); else Serial.print(" CW");  
-    Serial.println();
-    
-    m0.setDirection(w0_ccw ? DIR_CCW : DIR_CW);
-    m1.setDirection(w1_ccw ? DIR_CCW : DIR_CW);
-    m2.setDirection(w2_ccw ? DIR_CCW : DIR_CW);
-   
-    m0.setSpeed(w0_speed);
     m1.setSpeed(w1_speed);
     m2.setSpeed(w2_speed);
-  }
-  else{ // stick is idle, stop the wheels
-    m0.setSpeed(0);
-    m1.setSpeed(0);
-    m2.setSpeed(0);
-  }  
+    m3.setSpeed(w3_speed);
+
+    // TODO: for more erratic behavior, we could use the mX.setBrake(BRAKE_ON); at times, to get harder stops
+}
+
+// Convert angles to radians;
+float rad(float ang) {
+  return ang / 57.29578;
 }
 
